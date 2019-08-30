34b7f1c4b57b84ef627999b7441db2889aa00a20
tigerwow <644986705@qq.com>
2019-08-30 20:00:13 +0800

    fix

---
+++ exercise-tigerwow/exercise-model/src/main/java/com/tigerwow/enums/HelloEnum.java
@@ -0,0 +1,22 @@
+package com.tigerwow.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Created by wanhu 2019-08-30 17:33
+ */
+@Getter
+@NoArgsConstructor
+public enum HelloEnum {
+
+    /**
+     *
+     */
+    RED,
+    /**
+     *
+     */
+    BLUE;
+}
--- exercise-tigerwow/exercise-model/src/main/java/com/tigerwow/vo/HelloVo.java
+++ exercise-tigerwow/exercise-model/src/main/java/com/tigerwow/vo/HelloVo.java
@@ -11,6 +11,5 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class HelloVo {
 }
