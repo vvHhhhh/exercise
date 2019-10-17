647ddece8964a36dd974dc4643fb3e8c2af8b9fa
tigerwow <vvhhh@vvHhhdeMBP.lan>
2019-10-17 20:24:12 +0800

    fix

--- exercise-tigerwow/exercise-provider/src/main/java/com/tigerwow/tea/Application.java
+++ exercise-tigerwow/exercise-provider/src/main/java/com/tigerwow/tea/Application.java
@@ -2,11 +2,15 @@ package com.tigerwow.tea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Created by wanhu 2019-08-30 10:43
  */
 @SpringBootApplication
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ exercise-tigerwow/exercise-provider/src/test/java/com/tigerwow/tea/TeaTest.java
@@ -0,0 +1,16 @@
+package com.tigerwow.tea;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author Created by wanhu 2019-09-02 17:54
+ */
+@Slf4j
+public class TeaTest extends BaseTest {
+
+    @Test
+    public void test1() {
+        log.info("正在测试中......");
+    }
+}
